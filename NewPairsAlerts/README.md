269a5641b33a06f4f8a866785fc628defe59ef32
SFW3B <110057312+SFW3B@users.noreply.github.com>
2023-05-04 20:44:51 -0400

    NewPairsAlerts

---
+++ NewPairsAlerts/README.md
@@ -0,0 +1,22 @@
+## NewPairsAlerts
+
+This tool send you Telegram Alerts when a new Uniswap/Sushiswap pair is created.
+
+## Install Dependencies:
+
+```node
+npm install web3 dotenv node-telegram-bot-api
+```
+
+## Usage
+
+Rename .env-example to .env and edit your API Keys and Telegram Chat ID, then run:
+
+```node
+node index.js
+```
+
+
+## Authors
+
+- [@SFW3B](https://www.github.com/SFW3B)
---
+++ NewPairsAlerts/index.js
@@ -0,0 +1,48 @@
+require('dotenv').config();
+const Web3 = require('web3');
+const TelegramBot = require('node-telegram-bot-api');
+
+const infuraProjectId = process.env.INFURA_PROJECT_ID;
+const telegramBotToken = process.env.TELEGRAM_BOT_TOKEN;
+const telegramChatId = process.env.TELEGRAM_CHAT_ID;
+
+const web3 = new Web3(new Web3.providers.WebsocketProvider(`wss://mainnet.infura.io/ws/v3/${infuraProjectId}`));
+const bot = new TelegramBot(telegramBotToken, { polling: false });
+
+const uniswapFactoryAddress = '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f';
+const sushiswapFactoryAddress = '0xC0AEe478e3658e2610c5F7A4A2E1777cE9e4f2Ac';
+
+const uniswapPairCreatedTopic = web3.utils.sha3('PairCreated(address,address,address,uint256)');
+const sushiswapPairCreatedTopic = web3.utils.sha3('PairCreated(address,address,address,uint256)');
+
+async function sendTelegramMessage(message) {
+  try {
+    await bot.sendMessage(telegramChatId, message);
+  } catch (error) {
+    console.error('Error sending Telegram message:', error);
+  }
+}
+
+sendTelegramMessage('🚀 New Listings Alert Bot Started 🚀');
+
+web3.eth.subscribe('logs', {
+  address: [uniswapFactoryAddress, sushiswapFactoryAddress],
+  topics: [],
+})
+  .on('connected', (subscriptionId) => {
+    console.log('Connected to logs with subscription ID:', subscriptionId);
+  })
+  .on('data', async (log) => {
+    console.log(log);
+    if (log.topics[0] === uniswapPairCreatedTopic || log.topics[0] === sushiswapPairCreatedTopic) {
+      const factory = log.address === uniswapFactoryAddress ? 'Uniswap' : 'Sushiswap';
+      const token0 = web3.eth.abi.decodeParameter('address', log.topics[1]);
+      const token1 = web3.eth.abi.decodeParameter('address', log.topics[2]);
+
+      const message = `🚀 New ${factory} Pool Created:\n\nToken 0: ${token0}\nToken 1: ${token1}`;
+      sendTelegramMessage(message);
+    }
+  })
+  .on('error', (error) => {
+    console.error('Error on subscription:', error);
+  });
